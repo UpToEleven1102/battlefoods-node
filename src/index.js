24e535eb596783028360672fda38da56705f56cd
Huyen Vu <huyen.vu@nextgencode.io>
2019-04-20 19:22:22 -0500

    connect to db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 
 import authRoutes from '../routes/auth'
 
 const app = express()
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true }, err => {
+    if (err)
+        console.log(err)
+    else console.log('Connected to DB')
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.use('/test', (req, res) => {
-    res.json({success: true, message: 'test route'})
+    res.json({ success: true, message: 'test route' })
 })
 
 app.use('/auth', authRoutes)
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT? process.env.PORT: '8080'}`))
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT ? process.env.PORT : '8080'}`))
